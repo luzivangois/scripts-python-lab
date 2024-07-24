fa09c523c92fb251509d24ad060547fc89ab30d6
Luzivan Gois <luzivangois@outlook.com>
2024-07-23 22:58:14 -0300

    Refatorado script para adequação de testes com API em NodeJS

--- bruteForce.py
+++ bruteForce.py
@@ -14,14 +14,14 @@ with open("usuarios_validos.txt", "r") as users_list:
                 user_password = password.strip()    
                 payload  = {'login': user_name, 'password': user_password}
 
-                # Request API em Java
-                # r = requests.post('http://localhost:8080/auth/login', json=payload)
+                # Request API em Java | Node
+                r = requests.post('http://localhost:8080/auth/login', json=payload)
 
                 # Request API em .Net
                 # r = requests.post('http://localhost:5127/api/authentication/login', json=payload)
 
-                # Request API em Java
-                r = requests.post('http://localhost:5000/user/login', json=payload)
+                # Request API em Python
+                # r = requests.post('http://localhost:5000/user/login', json=payload)
 
                 response = r.status_code
                 if (response == 200):
--- enumUsers.py
+++ enumUsers.py
@@ -8,16 +8,16 @@ with open("wordlist/users.txt", "r") as file:
         user_name  = line.strip()
         payload  = {'login': user_name, 'password': ''}
 
-        # Request API em Java
-        # r = requests.post('http://localhost:8080/auth/login', json=payload)  
+        # Request API em Java | Node
+        r = requests.post('http://localhost:8080/auth/login', json=payload)
 
         # Request API em .Net
         # r = requests.post('http://localhost:5127/api/authentication/login', json=payload)
 
         # Request API em Python
-        r = requests.post('http://localhost:5000/user/login', json=payload)
+        # r = requests.post('http://localhost:5000/user/login', json=payload)
 
         message = r.text
-        if (message == "Senha Incorreta!" or message == '{"message":"Wrong password!!!"}'):
+        if (message == "Senha Incorreta!" or message == '{"message":"Senha Incorreta!"}'):
             with open("usuarios_validos.txt", "a") as result:
                 result.write(user_name+"\n")
